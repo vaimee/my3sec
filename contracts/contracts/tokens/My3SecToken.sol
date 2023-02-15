4130d135a199e98345770a814829b26d9354c7a7
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-02-15 11:34:56 +0100

    feat: add My3SecToken contract and interface

---
+++ contracts/contracts/tokens/My3SecToken.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/**
+ * @title My3SecToken contract
+ * @dev This is the implementation of the ERC20 My3Sec Token.
+ * 
+ * The token is initially owned by the deployer address that can mint tokens to create the initial
+ * distribution. For convenience, an initial supply can be passed in the constructor that will be
+ * assigned to the deployer.
+ * 
+ * The My3Sec rewards system (to be defined) should be added as a minter to distribute rewards to the users.
+ */
+contract My3SecToken is ERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20Votes {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    /**
+     * @dev My3Sec Token Contract Constructor.
+     * @param initialSupply Initial supply of M3S
+     */
+    constructor(uint256 initialSupply) ERC20("My3Sec Token", "M3S") ERC20Permit("My3Sec Token") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    /**
+     * Mint new tokens.
+     * @param to Address to send the newly minted tokens
+     * @param amount Amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
