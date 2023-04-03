337e5592fec5358acbd8f691801dd5a44c75eea2
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-04-03 12:27:35 +0200

    refactor(protocol/test): align EnergyManager tests with profiles

--- protocol/test/reputation/EnergyManager.spec.ts
+++ protocol/test/reputation/EnergyManager.spec.ts
@@ -2,10 +2,13 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 
 import { EnergyManager } from "../../typechain-types";
-import { getRandomAddress, getTestResources, TestResources } from "../utils";
+import { getRandomProfileId, getTestResources, TestResources } from "../utils";
 
-const ACCOUNT_INITIAL_ENERGY = 100;
+const PROFILE_INITIAL_ENERGY = 100;
 const TOTAL_TEST_USERS = 5;
+
+const ENERGIZED_ID = 1;
+const ENERGIZER_ID = 2;
 
 describe("EnergyManager", () => {
   let resources: TestResources;
@@ -15,239 +18,213 @@ describe("EnergyManager", () => {
     const contractFactory = await ethers.getContractFactory("EnergyManager");
     contract = await contractFactory.deploy();
     resources = await getTestResources();
+
+    await contract.addToWhitelist(resources.deployerAddress);
   });
 
   describe("Deployment", () => {
     it("should have initial energy equals zero", async () => {
-      const { deployerAddress } = resources;
-      const deployerTotalEnergy = await contract.totalEnergyOf(deployerAddress);
-      expect(deployerTotalEnergy).to.be.equal(0);
+      const totalEnergy = await contract.totalEnergyOf(ENERGIZED_ID);
+      expect(totalEnergy).to.be.equal(0);
     });
 
     it("should have initial free energy equals to total energy", async () => {
-      const { deployerAddress } = resources;
-      const deployerTotalEnergy = await contract.totalEnergyOf(deployerAddress);
-      const deployerFreeEnergy = await contract.freeEnergyOf(resources.deployerAddress);
-      expect(deployerTotalEnergy).to.be.equal(deployerFreeEnergy);
+      const totalEnergy = await contract.totalEnergyOf(ENERGIZED_ID);
+      const freeEnergy = await contract.freeEnergyOf(ENERGIZED_ID);
+      expect(totalEnergy).to.be.equal(freeEnergy);
     });
 
     it("should have initial allocated energy equals to zero", async () => {
-      const { deployerAddress } = resources;
-      const deployerAllocatedEnergy = await contract.allocatedEnergyOf(deployerAddress);
-      expect(deployerAllocatedEnergy).to.be.equal(0);
+      const allocatedEnergy = await contract.allocatedEnergyOf(ENERGIZED_ID);
+      expect(allocatedEnergy).to.be.equal(0);
     });
 
     it("should have initial received energy equals to zero", async () => {
-      const { deployerAddress } = resources;
-      const deployerReceivedEnergy = await contract.receivedEnergyOf(deployerAddress);
-      expect(deployerReceivedEnergy).to.be.equal(0);
+      const receivedEnergy = await contract.receivedEnergyOf(ENERGIZED_ID);
+      expect(receivedEnergy).to.be.equal(0);
     });
   });
 
   describe("createEnergyFor", () => {
-    it("should create energy for an address", async () => {
-      const receiverAddress = getRandomAddress();
-      const energy = ACCOUNT_INITIAL_ENERGY;
+    it("should create energy for a profile", async () => {
+      const energy = PROFILE_INITIAL_ENERGY;
 
-      await contract.createEnergyFor(receiverAddress, energy);
+      await contract.createEnergyFor(ENERGIZED_ID, energy);
 
-      const receiverTotalEnergy = await contract.totalEnergyOf(receiverAddress);
-      const receiverFreeEnergy = await contract.freeEnergyOf(receiverAddress);
-      const receiverAllocatedEnergy = await contract.allocatedEnergyOf(receiverAddress);
+      const totalEnergy = await contract.totalEnergyOf(ENERGIZED_ID);
+      const freeEnergy = await contract.freeEnergyOf(ENERGIZED_ID);
+      const allocatedEnergy = await contract.allocatedEnergyOf(ENERGIZED_ID);
 
-      expect(receiverTotalEnergy).to.be.equal(energy);
-      expect(receiverFreeEnergy).to.be.equal(energy);
-      expect(receiverAllocatedEnergy).to.be.equal(0);
+      expect(totalEnergy).to.be.equal(energy);
+      expect(freeEnergy).to.be.equal(energy);
+      expect(allocatedEnergy).to.be.equal(0);
     });
   });
 
   describe("destroyEnergyFor", () => {
     it("should fail to give energy to himself", async () => {
-      const [_, energizer] = await ethers.getSigners();
       const energy = 10;
 
-      const tx = contract.connect(energizer).giveEnergyTo(energizer.address, energy);
+      const tx = contract.giveEnergy(ENERGIZER_ID, ENERGIZER_ID, energy);
 
       await expect(tx).to.be.revertedWith("Cannot give energy to yourself");
     });
 
     it("should fail to give too much energy", async () => {
-      const [_, energizer] = await ethers.getSigners();
-      const energizedAddress = getRandomAddress();
       const energy = 1000;
 
-      const tx = contract.connect(energizer).giveEnergyTo(energizedAddress, energy);
+      const tx = contract.giveEnergy(ENERGIZER_ID, ENERGIZED_ID, energy);
 
       await expect(tx).to.be.revertedWith("Insufficient energy");
     });
 
-    it("should remove energy from an address", async () => {
-      const targetAddress = getRandomAddress();
-      const energy = ACCOUNT_INITIAL_ENERGY;
-      await contract.createEnergyFor(targetAddress, energy);
+    it("should remove energy from a profile", async () => {
+      const energy = PROFILE_INITIAL_ENERGY;
+      await contract.createEnergyFor(ENERGIZED_ID, energy);
 
-      await contract.destroyEnergyFor(targetAddress, energy);
+      await contract.destroyEnergyFor(ENERGIZED_ID, energy);
 
-      const targetTotalEnergy = await contract.totalEnergyOf(targetAddress);
-      const targetFreeEnergy = await contract.freeEnergyOf(targetAddress);
-      const targetAllocatedEnergy = await contract.allocatedEnergyOf(targetAddress);
+      const totalEnergy = await contract.totalEnergyOf(ENERGIZED_ID);
+      const freeEnergy = await contract.freeEnergyOf(ENERGIZED_ID);
+      const allocatedEnergy = await contract.allocatedEnergyOf(ENERGIZED_ID);
 
-      expect(targetTotalEnergy).to.be.equal(0);
-      expect(targetFreeEnergy).to.be.equal(0);
-      expect(targetAllocatedEnergy).to.be.equal(0);
+      expect(totalEnergy).to.be.equal(0);
+      expect(freeEnergy).to.be.equal(0);
+      expect(allocatedEnergy).to.be.equal(0);
     });
   });
 
-  describe("giveEnergyTo", () => {
+  describe("giveEnergy", () => {
     beforeEach(async () => {
-      const [_, energizer] = await ethers.getSigners();
-
-      await contract.createEnergyFor(energizer.address, ACCOUNT_INITIAL_ENERGY);
-      contract.connect(energizer);
+      await contract.createEnergyFor(ENERGIZER_ID, PROFILE_INITIAL_ENERGY);
     });
 
-    it("should give energy to another address", async () => {
-      const [_, energizer] = await ethers.getSigners();
-      const energizerAddress = energizer.address;
-      const energizedAddress = getRandomAddress();
+    it("should give energy to another profile", async () => {
       const energy = 10;
 
-      await contract.connect(energizer).giveEnergyTo(energizedAddress, energy);
+      await contract.giveEnergy(ENERGIZER_ID, ENERGIZED_ID, energy);
 
-      const energizedReceivedEnergy = await contract.receivedEnergyOf(energizedAddress);
-      const energizerFreeEnergy = await contract.freeEnergyOf(energizerAddress);
-      const energizerTotalEnergy = await contract.totalEnergyOf(energizerAddress);
-      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(energizerAddress);
-      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(energizerAddress);
-      const totalEnergizersOfEnergized = await contract.totalEnergizersOf(energizedAddress);
-      const [retrievedEnergizedAddress, retrievedEnergizedEnergy] = await contract.energizedBy(energizerAddress, 0);
-      const [retrievedEnergizerAddress, retrievedEnergizerEnergy] = await contract.energizersOf(energizedAddress, 0);
+      const energizedReceivedEnergy = await contract.receivedEnergyOf(ENERGIZED_ID);
+      const energizerFreeEnergy = await contract.freeEnergyOf(ENERGIZER_ID);
+      const energizerTotalEnergy = await contract.totalEnergyOf(ENERGIZER_ID);
+      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(ENERGIZER_ID);
+      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(ENERGIZER_ID);
+      const totalEnergizersOfEnergized = await contract.totalEnergizersOf(ENERGIZED_ID);
+      const [energizedProfileId, energizedEnergy] = await contract.energizedBy(ENERGIZER_ID, 0);
+      const [energizerProfileId, energizerEnergy] = await contract.energizersOf(ENERGIZED_ID, 0);
 
       expect(energizedReceivedEnergy).to.be.equal(energy);
       expect(energizerFreeEnergy).to.be.equal(energizerTotalEnergy.sub(energy));
       expect(energizerAllocatedEnergy).to.be.equal(energy);
       expect(totalEnergizedByEnergizer).to.be.equal(1);
       expect(totalEnergizersOfEnergized).to.be.equal(1);
-      expect(retrievedEnergizedAddress).to.be.equal(energizedAddress);
-      expect(retrievedEnergizedEnergy).to.be.equal(energy);
-      expect(retrievedEnergizerAddress).to.be.equal(energizerAddress);
-      expect(retrievedEnergizerEnergy).to.be.equal(energy);
+      expect(energizedProfileId).to.be.equal(ENERGIZED_ID);
+      expect(energizedEnergy).to.be.equal(energy);
+      expect(energizerProfileId).to.be.equal(ENERGIZER_ID);
+      expect(energizerEnergy).to.be.equal(energy);
     });
 
-    it("should give energy to multiple addresses", async () => {
-      const [_, energizer] = await ethers.getSigners();
-      const energizerAddress = energizer.address;
+    it("should give energy to multiple profiles", async () => {
       const energy = 10;
 
       for (let i = 0; i < TOTAL_TEST_USERS; i++) {
-        const receiverAddress = getRandomAddress();
+        const energized = getRandomProfileId();
 
-        await contract.connect(energizer).giveEnergyTo(receiverAddress, energy);
+        await contract.giveEnergy(ENERGIZER_ID, energized, energy);
 
-        const receiverReceivedEnergy = await contract.receivedEnergyOf(receiverAddress);
-        const [energizedAddress, energizedEnergy] = await contract.energizedBy(energizerAddress, i);
+        const energizedReceivedEnergy = await contract.receivedEnergyOf(energized);
+        const [energizedProfileId, energizedEnergy] = await contract.energizedBy(ENERGIZER_ID, i);
 
-        expect(receiverReceivedEnergy).to.be.equal(energy);
-        expect(energizedAddress).to.be.equal(receiverAddress);
+        expect(energizedReceivedEnergy).to.be.equal(energy);
+        expect(energizedProfileId).to.be.equal(energized);
         expect(energizedEnergy).to.be.equal(energy);
       }
 
-      const energizerFreeEnergy = await contract.freeEnergyOf(energizerAddress);
-      const energizerTotalEnergy = await contract.totalEnergyOf(energizerAddress);
-      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(energizerAddress);
-      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(energizerAddress);
+      const energizerFreeEnergy = await contract.freeEnergyOf(ENERGIZER_ID);
+      const energizerTotalEnergy = await contract.totalEnergyOf(ENERGIZER_ID);
+      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(ENERGIZER_ID);
+      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(ENERGIZER_ID);
 
       expect(energizerFreeEnergy).to.be.equal(energizerTotalEnergy.sub(energy * TOTAL_TEST_USERS));
       expect(energizerAllocatedEnergy).to.be.equal(energy * TOTAL_TEST_USERS);
       expect(totalEnergizedByEnergizer).to.be.equal(TOTAL_TEST_USERS);
     });
 
-    it("should receive energy from multiple addresses", async () => {
-      const signers = await ethers.getSigners();
-      const receiverAddress = getRandomAddress();
+    it("should receive energy from multiple profiles", async () => {
       const energy = 10;
+      const energizers = [];
 
       for (let i = 0; i < TOTAL_TEST_USERS; i++) {
-        await contract.createEnergyFor(signers[i].address, ACCOUNT_INITIAL_ENERGY);
+        const energizer = getRandomProfileId();
+        energizers.push(energizer);
 
-        await contract.connect(signers[i]).giveEnergyTo(receiverAddress, energy);
+        await contract.createEnergyFor(energizer, PROFILE_INITIAL_ENERGY);
 
-        const receiverReceivedEnergy = await contract.receivedEnergyOf(receiverAddress);
-        const senderAllocatedEnergy = await contract.allocatedEnergyOf(signers[i].address);
-        const [energizerAddress, energizerEnergy] = await contract.energizersOf(receiverAddress, i);
+        await contract.giveEnergy(energizer, ENERGIZED_ID, energy);
 
-        expect(receiverReceivedEnergy).to.be.equal(energy * (i + 1));
+        const energizedReceivedEnergy = await contract.receivedEnergyOf(ENERGIZED_ID);
+        const senderAllocatedEnergy = await contract.allocatedEnergyOf(energizer);
+        const [energizerProfileId, energizerEnergy] = await contract.energizersOf(ENERGIZED_ID, i);
+
+        expect(energizedReceivedEnergy).to.be.equal(energy * (i + 1));
         expect(senderAllocatedEnergy).to.be.equal(energy);
-        expect(energizerAddress).to.be.equal(signers[i].address);
+        expect(energizerProfileId).to.be.equal(energizer);
         expect(energizerEnergy).to.be.equal(energy);
       }
 
-      const totalEnergizersOfReceiver = await contract.totalEnergizersOf(receiverAddress);
+      const totalEnergizersOfReceiver = await contract.totalEnergizersOf(ENERGIZED_ID);
       expect(totalEnergizersOfReceiver).to.be.equal(TOTAL_TEST_USERS);
     });
   });
 
-  describe("removeEnergyFrom", () => {
+  describe("removeEnergy", () => {
     const INITIAL_GIVEN_ENERGY = 90;
 
-    let energizerAddress: string;
-    let energizedAddress: string;
-
     beforeEach(async () => {
-      const [_, energizer] = await ethers.getSigners();
-
-      await contract.createEnergyFor(energizer.address, ACCOUNT_INITIAL_ENERGY);
-      contract.connect(energizer);
-
-      energizerAddress = energizer.address;
-      energizedAddress = getRandomAddress();
-
-      await contract.connect(energizer).giveEnergyTo(energizedAddress, INITIAL_GIVEN_ENERGY);
+      await contract.createEnergyFor(ENERGIZER_ID, PROFILE_INITIAL_ENERGY);
+      await contract.giveEnergy(ENERGIZER_ID, ENERGIZED_ID, INITIAL_GIVEN_ENERGY);
     });
 
-    it("should fail to remove energy from a not energized address", async () => {
-      const [_, energizer] = await ethers.getSigners();
-      const targetAddress = getRandomAddress();
+    it("should fail to remove energy from a not energized profile", async () => {
+      const notEnergized = getRandomProfileId();
       const energy = 10;
 
-      const tx = contract.connect(energizer).removeEnergyFrom(targetAddress, energy);
+      const tx = contract.removeEnergy(notEnergized, ENERGIZER_ID, energy);
 
-      await expect(tx).to.be.revertedWith("Account not referenced");
+      await expect(tx).to.be.revertedWith("Profile not referenced");
     });
 
     it("should fail to remove more energy that the already allocated", async () => {
-      const [_, energizer] = await ethers.getSigners();
       const energy = 10000;
 
-      const tx = contract.connect(energizer).removeEnergyFrom(energizedAddress, energy);
+      const tx = contract.removeEnergy(ENERGIZED_ID, ENERGIZER_ID, energy);
 
       await expect(tx).to.be.revertedWith("Exceeded given energy");
     });
 
-    it("should remove energy from an already energized address", async () => {
-      const [_, energizer] = await ethers.getSigners();
+    it("should remove energy from an already energized profile", async () => {
       const energy = 10;
 
-      await contract.connect(energizer).removeEnergyFrom(energizedAddress, energy);
+      await contract.removeEnergy(ENERGIZED_ID, ENERGIZER_ID, energy);
 
-      const energizedReceivedEnergy = await contract.receivedEnergyOf(energizedAddress);
-      const energizerFreeEnergy = await contract.freeEnergyOf(energizerAddress);
-      const energizerTotalEnergy = await contract.totalEnergyOf(energizerAddress);
-      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(energizerAddress);
-      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(energizerAddress);
-      const totalEnergizersOfEnergized = await contract.totalEnergizersOf(energizedAddress);
-      const [retrievedEnergizedAddress, retrievedEnergizedEnergy] = await contract.energizedBy(energizerAddress, 0);
-      const [retrievedEnergizerAddress, retrievedEnergizerEnergy] = await contract.energizersOf(energizedAddress, 0);
+      const energizedReceivedEnergy = await contract.receivedEnergyOf(ENERGIZED_ID);
+      const energizerFreeEnergy = await contract.freeEnergyOf(ENERGIZER_ID);
+      const energizerTotalEnergy = await contract.totalEnergyOf(ENERGIZER_ID);
+      const energizerAllocatedEnergy = await contract.allocatedEnergyOf(ENERGIZER_ID);
+      const totalEnergizedByEnergizer = await contract.totalEnergizedBy(ENERGIZER_ID);
+      const totalEnergizersOfEnergized = await contract.totalEnergizersOf(ENERGIZED_ID);
+      const [energizedProfileId, energizedEnergy] = await contract.energizedBy(ENERGIZER_ID, 0);
+      const [energizerProfileId, energizerEnergy] = await contract.energizersOf(ENERGIZED_ID, 0);
 
       expect(energizedReceivedEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
       expect(energizerFreeEnergy).to.be.equal(energizerTotalEnergy.sub(INITIAL_GIVEN_ENERGY).add(energy));
       expect(energizerAllocatedEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
       expect(totalEnergizedByEnergizer).to.be.equal(1);
       expect(totalEnergizersOfEnergized).to.be.equal(1);
-      expect(retrievedEnergizedAddress).to.be.equal(energizedAddress);
-      expect(retrievedEnergizedEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
-      expect(retrievedEnergizerAddress).to.be.equal(energizerAddress);
-      expect(retrievedEnergizerEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
+      expect(energizedProfileId).to.be.equal(ENERGIZED_ID);
+      expect(energizedEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
+      expect(energizerProfileId).to.be.equal(ENERGIZER_ID);
+      expect(energizerEnergy).to.be.equal(INITIAL_GIVEN_ENERGY - energy);
     });
   });
 });
--- protocol/test/utils.ts
+++ protocol/test/utils.ts
@@ -19,3 +19,7 @@ export async function getTestResources(): Promise<TestResources> {
 export function getRandomAddress(): string {
   return ethers.Wallet.createRandom().address;
 }
+
+export function getRandomProfileId(): number {
+  return Math.floor(Math.random() * 10000) + 10;
+}
