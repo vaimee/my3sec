244c4bba42668c06913fd40adc25c4ea0ecc0ffe
Kumar Guarav <kumardeveloper80@gmail.com>
2023-04-10 14:20:12 +0530

    navbar toggle fixed

--- frontend/src/app/componets/navbar/navbar.component.ts
+++ frontend/src/app/componets/navbar/navbar.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  
+
 })
 export class NavbarComponent {
 
