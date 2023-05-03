fda5b8fcb26e99760b14950eb8a5e6f9a7e9d8ea
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-05-03 15:45:58 +0200

    chore(protocol): update deploy script

--- protocol/scripts/deploy.ts
+++ protocol/scripts/deploy.ts
@@ -1,20 +1,49 @@
 import { writeFileSync } from "fs";
 import { ethers } from "hardhat";
 
+const MY3SEC_TOKEN_INITIAL_SUPPLY = 10000000;
+
 async function main() {
-  const My3SecHub = await ethers.getContractFactory("My3SecHub");
-  const my3secHub = await My3SecHub.deploy();
+  // Factories
+  const my3secHubFactory = await ethers.getContractFactory("My3SecHub");
+  const my3secTokenFactory = await ethers.getContractFactory("My3SecToken");
+  const my3secProfilesFactory = await ethers.getContractFactory("My3SecProfiles");
+  const energyManagerFactory = await ethers.getContractFactory("EnergyManager");
 
-  await my3secHub.deployed();
+  // Deployments
+  console.log("\n\t-- Deploying My3SecHub --");
+  const my3secHub = await my3secHubFactory.deploy();
 
-  console.log(`My3SecHub deployed to ${my3secHub.address}`);
+  console.log("\n\t-- Deploying My3SecToken --");
+  const my3secToken = await my3secTokenFactory.deploy(MY3SEC_TOKEN_INITIAL_SUPPLY);
 
-  writeFileSync(
-    "./deployed.json",
-    JSON.stringify({
-      my3sec: my3secHub.address,
-    })
-  );
+  console.log("\n\t-- Deploying My3SecProfiles --");
+  const my3secProfiles = await my3secProfilesFactory.deploy();
+
+  console.log("\n\t-- Deploying EnergyManager --");
+  const energyManager = await energyManagerFactory.deploy();
+
+  // Initializations
+  console.log("\n\t-- Initializing My3SecHub --");
+  await my3secHub.setMy3SecProfilesContract(my3secProfiles.address);
+  await my3secHub.setEnergyManagerContract(energyManager.address);
+
+  console.log("\n\t-- Whitelisting contracts --");
+  await my3secToken.addToWhitelist(my3secHub.address);
+  await my3secProfiles.addToWhitelist(my3secHub.address);
+  await energyManager.addToWhitelist(my3secHub.address);
+
+  // Save and logs addresses
+  const addrs = {
+    my3secHub: my3secHub.address,
+    my3secToken: my3secToken.address,
+    my3secProfiles: my3secProfiles.address,
+    energyManager: energyManager.address,
+  };
+  const json = JSON.stringify(addrs, null, 2);
+  console.log(json);
+
+  writeFileSync("./deployed.json", json, "utf-8");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
