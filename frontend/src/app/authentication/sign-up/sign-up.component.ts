19b151574ae6b15bdb60ed488be23769ea36f668
ivan <idimitryz@gmail.com>
2023-05-08 18:26:26 +0200

    feat(frontend): wire signup with contract call
    when the user sign up, the createProfile function from My3SecHub contract is called. Still need to define the uri passed as argument of the createProfile function.

--- frontend/src/app/authentication/sign-up/sign-up.component.ts
+++ frontend/src/app/authentication/sign-up/sign-up.component.ts
@@ -1,42 +1,41 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { My3secHubContractService } from 'app/shared/services/my3sec-hub-contract.service';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
 })
 export class SignUpComponent implements OnInit {
   formGroup!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _my3secHub: My3secHubContractService
+  ) {}
 
   ngOnInit(): void {
     this.loadForm();
   }
 
-  //Load form
   loadForm() {
-    this.formGroup = this.fb.group({
+    this.formGroup = this._formBuilder.group({
       firstName: ['', Validators.compose([Validators.required])],
-      surName: ['', Validators.compose([Validators.required])],
+      surname: ['', Validators.compose([Validators.required])],
       organization: ['', Validators.compose([Validators.required])],
       role: ['', Validators.compose([Validators.required])],
-      cb: [false, Validators.requiredTrue]
+      cb: [false, Validators.requiredTrue],
     });
   }
 
-  onSubmit() {
-    if (this.formGroup.valid) {
-      console.table(this.formGroup.value);
-    }
+  async onSubmit() {
+    if (!this.formGroup.valid) return;
+    //TODO: structure the uri passed
+    this._my3secHub.createProfile(this.formGroup.value)
   }
 
-  // helpers for View
   public myError = (controlName: string, errorName: string) => {
     return this.formGroup.controls[controlName].hasError(errorName);
-  }
-
-  get f() { return this.formGroup.controls; }
+  };
 }
