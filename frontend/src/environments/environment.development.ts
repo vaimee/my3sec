62063c2c4cf8be434e8e597b20b472351aea5385
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-05-15 18:29:08 +0200

    chore(frontend): add hardcoded ipfs key to dev env

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -19,5 +19,8 @@ export const environment = {
   abiPaths: {
     my3secHub: '../../../assets/contracts/my3sec-hub.abi.json',
   },
-  ipfs: { httpGateway: 'https://ipfs.io/ipfs', api_key: process.env["MY3SEC_IPFS_API_KEY"]},
+  ipfs: { 
+    httpGateway: 'https://ipfs.io/ipfs', 
+    api_key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGIwMmQ4OEViZmQwOTM5N2M5NUNDNkJFYTUxOTliMDYwZEQyQzZmNjAiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY4Mzg4Nzc0NzQ0NCwibmFtZSI6Im15M3NlYyJ9.7ICKTPfgxcnBSblJQ_4ptfaQm82wLpgY4flozHkCKzs" 
+  },
 };
