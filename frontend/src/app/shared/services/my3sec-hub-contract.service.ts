944aef4a0a7e995bf5822e41e170aa94850d35af
reluc <relu.cri@gmail.com>
2023-06-10 18:59:18 +0200

    feat(frontend): use my3sec contracts

--- frontend/src/app/shared/services/my3sec-hub-contract.service.ts
+++ frontend/src/app/shared/services/my3sec-hub-contract.service.ts
@@ -4,6 +4,7 @@ import { ContractTransaction, ethers, providers } from 'ethers';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { Observable, finalize, from, map, switchMap } from 'rxjs';
+import {My3SecHub, My3SecHub__factory} from '@vaimee/my3sec-contracts/typechain-types';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +13,7 @@ export class My3secHubContractService {
   private contractAddress = environment.contracts.my3secHub;
   private provider: ethers.providers.JsonRpcProvider;
   private signer: ethers.Signer;
-  private contract!: ethers.Contract;
+  private contract: My3SecHub;
 
   constructor(
     private http: HttpClient,
@@ -24,24 +25,12 @@ export class My3secHubContractService {
     );
 
     this.signer = this.provider.getSigner();
-    this.http
-      .get<ethers.ContractInterface>(environment.abiPaths.my3secHub)
-      .subscribe((abi) => {
-        this.contract = new ethers.Contract(
-          this.contractAddress,
-          abi,
-          this.signer
-        );
-      });
+    const My3SecHub__factory = require("@vaimee/my3sec-contracts/typechain-types").My3SecHub__factory;
+    
+    this.contract = My3SecHub__factory.connect(this.contractAddress, this.signer);
   }
   public getDefaultProfile(account: string): Observable<string[]> {
     this.loadingService.show();
-    if (this.contract === undefined)
-      return this.initializeContract().pipe(
-        switchMap(() => from(this.contract['getDefaultProfile'](account))),
-        map((value) => value as string[]),
-        finalize(() => this.loadingService.hide())
-      );
 
     return from(this.contract['getDefaultProfile'](account)).pipe(
       map((value) => value as string[]),
@@ -53,9 +42,9 @@ export class My3secHubContractService {
     return from(this.contract['getProfile'](profileId));
   }
 
-  public createProfile(uri: string): Observable<number> {
+  public createProfile(metadataURI: string): Observable<number> {
     this.loadingService.show();
-    const args = { uri };
+    const args = { metadataURI };
 
     return from(this.contract['createProfile'](args)).pipe(
       switchMap((tx) =>
@@ -88,17 +77,4 @@ export class My3secHubContractService {
     return from(this.contract['removeEnergyFrom'](profileId, amount));
   }
 
-  private initializeContract() {
-    return this.http
-      .get<ethers.ContractInterface>(environment.abiPaths.my3secHub)
-      .pipe(
-        map((abi) => {
-          this.contract = new ethers.Contract(
-            this.contractAddress,
-            abi,
-            this.signer
-          );
-        })
-      );
-  }
 }
