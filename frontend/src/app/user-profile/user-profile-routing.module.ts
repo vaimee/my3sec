14dcf3ba15b2b78d008786af9ed9414de82aef16
ivan <idimitryz@gmail.com>
2023-06-14 10:59:07 +0200

    fix(frontend): remove duplicate 'profile' in the uri

--- frontend/src/app/user-profile/user-profile-routing.module.ts
+++ frontend/src/app/user-profile/user-profile-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileBodyComponent } from './components/profile-body/profile-body.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'profile/my-profile', pathMatch: 'full' },
-  { path: 'profile/my-profile', component: ProfileBodyComponent, data: { useDefaultProfile: true } },
-  { path: 'profile/:userId', component: ProfileBodyComponent, data: { useDefaultProfile: false } },
+  { path: '', redirectTo: 'my-profile', pathMatch: 'full' },
+  { path: 'my-profile', component: ProfileBodyComponent, data: { useDefaultProfile: true } },
+  { path: ':userId', component: ProfileBodyComponent, data: { useDefaultProfile: false } },
 ];
 
 @NgModule({
