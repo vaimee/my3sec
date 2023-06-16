16c477c13bfb137b7c34f600baac7f7a46e7bc1e
reluc <relu.cri@gmail.com>
2023-06-16 17:55:47 +0200

    refactor(frontend/organization): clean up create component from profile terms

--- frontend/src/app/modules/organizations/components/create/create-organization.component.ts
+++ frontend/src/app/modules/organizations/components/create/create-organization.component.ts
@@ -1,10 +1,6 @@
-import { Observable, catchError, finalize, map, of, switchMap } from 'rxjs';
-
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-import { MetamaskService } from '@auth/services/metamask.service';
 
 import { ImageConversionService } from '@shared/services/image-conversion.service';
 import { IpfsService } from '@shared/services/ipfs.service';
@@ -16,16 +12,14 @@ import { My3secHubContractService } from '@shared/services/my3sec-hub-contract.s
   templateUrl: './create-organization.component.html',
   styleUrls: ['./create-organization.component.css'],
 })
-export class CreateOrganizationComponent implements OnInit {
+export class CreateOrganizationComponent implements OnInit, OnDestroy {
   signUpForm!: FormGroup;
   submitted = false;
-  profileExists$!: Observable<boolean>;
   base64Image = '';
   constructor(
     private formBuilder: FormBuilder,
     private my3secHubContractService: My3secHubContractService,
     private ipfsService: IpfsService,
-    private metamaskService: MetamaskService,
     private imageConversionService: ImageConversionService,
     private loadingService: LoadingService,
     private router: Router
@@ -37,40 +31,18 @@ export class CreateOrganizationComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadForm();
-
-    this.profileExists$ = this.my3secHubContractService.getDefaultProfile(this.metamaskService.userAddress).pipe(
-      map(value => {
-        if (value === undefined) return false;
-        return true;
-      }),
-      catchError(error => {
-        console.error(error);
-        console.log('error when reading profile - redirect to signup');
-        return of(false);
-      }),
-      finalize(() => {
-        this.loadingService.hide();
-      })
-    );
   }
 
   loadForm() {
     this.signUpForm = this.formBuilder.group({
-      firstName: ['', Validators.compose([Validators.required])],
-      surname: ['', Validators.compose([Validators.required])],
-      organization: ['', Validators.compose([Validators.required])],
-      role: ['', Validators.compose([Validators.required])],
-      profileImage: [null, Validators.compose([Validators.required])],
-      regulationCheckbox: [false, Validators.requiredTrue],
+      name: ['', Validators.compose([Validators.required])],
+      description: ['', Validators.compose([Validators.required])],
+      organizationIcon: [null, Validators.compose([Validators.required])],
     });
   }
 
-  get profileImage() {
-    return this.signUpForm.get('profileImage');
-  }
-
-  get regulationCheckbox() {
-    return this.signUpForm.get('regulationCheckbox');
+  get organizationIcon() {
+    return this.signUpForm.get('organizationIcon');
   }
 
   async onSubmit() {
@@ -83,7 +55,7 @@ export class CreateOrganizationComponent implements OnInit {
     this.loadingService.show();
     this.ipfsService
       .storeJSON(formValue)
-      .pipe(switchMap(uri => this.my3secHubContractService.createProfile(uri)))
+      // TODO - create organization
       .subscribe();
   }
 
