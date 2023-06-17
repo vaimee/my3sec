c190da665e8691af25b097d4cadcd03b40a8a17e
ivan <idimitryz@gmail.com>
2023-06-17 21:16:01 +0200

    feat(frontend): if path is profiles/ without parameters, redirect to user home page

--- frontend/src/app/modules/profiles/profiles-routing.module.ts
+++ frontend/src/app/modules/profiles/profiles-routing.module.ts
@@ -4,6 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileBodyComponent } from './components/profile-body/profile-body.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'me',
+    pathMatch: 'full',
+  },
   { path: 'me', component: ProfileBodyComponent, data: { useDefaultProfile: true } },
   { path: ':userId', component: ProfileBodyComponent, data: { useDefaultProfile: false } },
 ];
