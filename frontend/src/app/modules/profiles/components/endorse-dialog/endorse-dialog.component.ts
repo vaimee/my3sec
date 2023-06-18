ee5e1553c41ecd589ec472e95c0201153aeb36f4
ivan <idimitryz@gmail.com>
2023-06-18 15:11:32 +0200

    feat(frontend): add loading when endorse someone

--- frontend/src/app/modules/profiles/components/endorse-dialog/endorse-dialog.component.ts
+++ frontend/src/app/modules/profiles/components/endorse-dialog/endorse-dialog.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+import { LoadingService } from '@shared/services/loading.service';
 import { My3secHubContractService } from '@shared/services/my3sec-hub-contract.service';
 import { ProfileService } from '@shared/services/profile.service';
 
@@ -20,12 +21,11 @@ export class EndorseDialogComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: EndorseDialogInterface,
     private my3secHubContractService: My3secHubContractService,
-    private profileService: ProfileService
+    private profileService: ProfileService,
+    private loadingService: LoadingService
   ) {}
 
   ngOnInit(): void {
-    console.log(`getEnergyEndorsedTo(${this.data.endorserId}, ${this.data.endorsingId})`);
-
     this.currentEndorsing$ = this.profileService.getEnergyEndorsedTo(this.data.endorserId, this.data.endorsingId);
     this.currentEndorsing$.subscribe(value => {
       this.targetEnergyToEndorse = value;
@@ -44,8 +44,14 @@ export class EndorseDialogComponent implements OnInit {
   endorse(currentEndorsing: number): void {
     const valueToEndorse = this.targetEnergyToEndorse - currentEndorsing;
     if (valueToEndorse === 0) return;
+    this.loadingService.show();
     if (valueToEndorse > 0)
-      this.my3secHubContractService.giveEnergyTo(this.data.endorsingId, this.targetEnergyToEndorse);
-    else this.my3secHubContractService.removeEnergyFrom(this.data.endorsingId, -valueToEndorse);
+      this.my3secHubContractService
+        .giveEnergyTo(this.data.endorsingId, valueToEndorse) //TODO: how giveEnergy works?
+        .subscribe(() => this.loadingService.hide());
+    else
+      this.my3secHubContractService
+        .removeEnergyFrom(this.data.endorsingId, -valueToEndorse)
+        .subscribe(() => this.loadingService.hide());
   }
 }
