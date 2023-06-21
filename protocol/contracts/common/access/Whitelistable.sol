22864007b32461236c8d33132c7b6bbfde3d87e8
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-06-21 11:11:53 +0200

    feat(protocol): update Whitelistable

--- protocol/contracts/common/access/Whitelistable.sol
+++ protocol/contracts/common/access/Whitelistable.sol
@@ -1,27 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "../libraries/Errors.sol";
 
 abstract contract Whitelistable is Ownable {
-    mapping(address => bool) private _whitelist;
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    EnumerableSet.AddressSet private _whitelist;
 
     modifier onlyWhitelisted() {
-        if (!_whitelist[msg.sender]) revert Errors.NotWhitelisted();
+        if (!_whitelist.contains(msg.sender)) revert Errors.NotWhitelisted();
         _;
     }
 
-    function addToWhitelist(address account) external onlyOwner {
-        _whitelist[account] = true;
+    function getWhitelistCount() external view returns (uint256) {
+        return _whitelist.length();
     }
 
-    function removeFromWhitelist(address account) external onlyOwner {
-        _whitelist[account] = false;
+    function getWhitelistMember(uint256 index) external view returns (address) {
+        return _whitelist.at(index);
     }
 
     function isWhitelisted(address account) public view virtual returns (bool) {
-        return _whitelist[account];
+        return _whitelist.contains(account);
+    }
+
+    function addToWhitelist(address account) external onlyOwner {
+        _whitelist.add(account);
+    }
+
+    function removeFromWhitelist(address account) external onlyOwner {
+        _whitelist.remove(account);
     }
 }
