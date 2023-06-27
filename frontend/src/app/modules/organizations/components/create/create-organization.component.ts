dcf738bce81670fdebf20d3d99e45f6791db977f
ivan <idimitryz@gmail.com>
2023-06-27 21:31:02 +0200

    feat(frontend): create organization wireup

--- frontend/src/app/modules/organizations/components/create/create-organization.component.ts
+++ frontend/src/app/modules/organizations/components/create/create-organization.component.ts
@@ -2,10 +2,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { OrganizationMetadata } from '@shared/interfaces';
 import { ImageConversionService } from '@shared/services/image-conversion.service';
-import { IpfsService } from '@shared/services/ipfs.service';
 import { LoadingService } from '@shared/services/loading.service';
-import { My3secHubContractService } from '@shared/services/my3sec-hub-contract.service';
+import { OrganizationService } from '@shared/services/organization.service';
 
 @Component({
   selector: 'app-create-organization',
@@ -18,8 +18,7 @@ export class CreateOrganizationComponent implements OnInit, OnDestroy {
   base64Image = '';
   constructor(
     private formBuilder: FormBuilder,
-    private my3secHubContractService: My3secHubContractService,
-    private ipfsService: IpfsService,
+    private organizationService: OrganizationService,
     private imageConversionService: ImageConversionService,
     private loadingService: LoadingService,
     private router: Router
@@ -37,6 +36,7 @@ export class CreateOrganizationComponent implements OnInit, OnDestroy {
     this.signUpForm = this.formBuilder.group({
       name: ['', Validators.compose([Validators.required])],
       description: ['', Validators.compose([Validators.required])],
+      headline: ['', Validators.compose([Validators.required])],
       organizationIcon: [null, Validators.compose([Validators.required])],
     });
   }
@@ -48,15 +48,21 @@ export class CreateOrganizationComponent implements OnInit, OnDestroy {
   async onSubmit() {
     this.submitted = true;
     if (!this.signUpForm.valid) return;
-
     if (this.base64Image === '') return;
-    const formValue = { ...this.signUpForm.value };
-    formValue.organizationIcon = this.base64Image;
+    const formValue: OrganizationMetadata = { ...this.signUpForm.value };
+    formValue.icon = this.base64Image;
     this.loadingService.show();
-    this.ipfsService
-      .storeJSON(formValue)
-      // TODO: create organization
-      .subscribe();
+    this.organizationService.createOrganization(formValue).subscribe({
+      next: address => {
+        this.loadingService.hide();
+        this.router.navigate(['organizations', address]);
+      },
+      error: err => {
+        this.loadingService.hide();
+        console.error(`Failed to create organization`);
+        console.error(err);
+      },
+    });
   }
 
   async onFileSelected(fileInputEvent: Event, label: HTMLDivElement): Promise<void> {
@@ -70,9 +76,8 @@ export class CreateOrganizationComponent implements OnInit, OnDestroy {
   reset() {
     this.submitted = false;
   }
-  public formError = (controlName: string, errorName: string) => {
-    console.log(controlName, errorName);
 
+  public formError = (controlName: string, errorName: string) => {
     return this.signUpForm.controls[controlName].hasError(errorName);
   };
 }
