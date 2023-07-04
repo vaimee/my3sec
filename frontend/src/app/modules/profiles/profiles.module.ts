20ae2cb2c312f9a8a227dbf97c4589dad174fdd4
reluc <relu.cri@gmail.com>
2023-07-04 17:11:47 +0200

    fix(frontend/profile): projects list loading

--- frontend/src/app/modules/profiles/profiles.module.ts
+++ frontend/src/app/modules/profiles/profiles.module.ts
@@ -4,11 +4,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '@shared/shared.module';
 
+import { ProjectsModule } from '@projects/projects.module';
+
 import { CertificatesComponent } from './components/certificates/certificates.component';
 import { EndorseDialogComponent } from './components/endorse-dialog/endorse-dialog.component';
 import { EndorsersListComponent } from './components/endorsers-list/endorsers-list.component';
 import { ProfileBodyComponent } from './components/profile-body/profile-body.component';
-import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProfilesRoutingModule } from './profiles-routing.module';
 
@@ -17,10 +18,9 @@ import { ProfilesRoutingModule } from './profiles-routing.module';
     ProfileBodyComponent,
     SkillsComponent,
     CertificatesComponent,
-    ProjectsComponent,
     EndorseDialogComponent,
     EndorsersListComponent,
   ],
-  imports: [CommonModule, ProfilesRoutingModule, SharedModule, ReactiveFormsModule],
+  imports: [CommonModule, ProfilesRoutingModule, ProjectsModule, SharedModule, ReactiveFormsModule],
 })
 export class ProfilesModule {}
