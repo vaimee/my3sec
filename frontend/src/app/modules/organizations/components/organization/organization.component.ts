eec7b9ef613e4850ed61f5bcc5d25e94025409e8
reluc <relu.cri@gmail.com>
2023-07-05 18:26:00 +0200

    feat(frontend/organization): restyle org page

--- frontend/src/app/modules/organizations/components/organization/organization.component.ts
+++ frontend/src/app/modules/organizations/components/organization/organization.component.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, map } from 'rxjs';
 
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
@@ -7,6 +7,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Organization, Profile, Project } from '@shared/interfaces';
 import { LoadingService } from '@shared/services/loading.service';
+import { NavbarService } from '@shared/services/navbar.services';
 import { OrganizationService } from '@shared/services/organization.service';
 import { ProfileService } from '@shared/services/profile.service';
 
@@ -28,11 +29,13 @@ export class OrganizationComponent implements OnInit {
   isPendingMember$!: Observable<boolean>;
   isManager$!: Observable<boolean>;
   userId$!: Observable<number>;
+  userRole$!: Observable<'manager' | 'member' | 'manager&member' | 'pendingMember' | 'outsider'>;
 
   constructor(
     private organizationService: OrganizationService,
     private profileService: ProfileService,
     private loadingService: LoadingService,
+    private navBarService: NavbarService,
     private snackBar: MatSnackBar,
     private route: ActivatedRoute,
     private router: Router,
@@ -52,6 +55,18 @@ export class OrganizationComponent implements OnInit {
     this.isPendingMember$ = this.organizationService.isCurrentUserPendingMember();
     this.isManager$ = this.organizationService.isCurrentUserManager();
     this.userId$ = this.profileService.getUserId();
+    this.userRole$ = this.getUserRole();
+    this.navBarService.setMenuItems([
+      {
+        label: 'New Project',
+        icon: 'workspaces',
+      },
+    ]);
+    this.navBarService.getMenuClickedEvent().subscribe(item => {
+      if (item.label === 'New Project') {
+        this.goToCreateProject();
+      }
+    });
   }
 
   public joinOrganization() {
@@ -60,19 +75,6 @@ export class OrganizationComponent implements OnInit {
       next: () => this.handleObservable('Added to the pending members list'),
       error: err => this.handleObservable('Failed to add to the organization', err),
     });
-  }
-
-  //What is the right way to deal with observables as input?
-  public showAddMemberIcon(
-    isMember: boolean | null,
-    isManager: boolean | null,
-    pendingMembers: Profile[],
-    userId: number | null
-  ): boolean {
-    if (isManager || isMember) return false;
-    for (const member of pendingMembers) if (Number(member.id) === userId) return false;
-
-    return true;
   }
 
   public openMemberDialog(memberType: MemberType, members: Profile[], isManager: boolean | null): void {
@@ -109,6 +111,26 @@ export class OrganizationComponent implements OnInit {
       error: err => this.handleObservable('failed to leave member', err),
     });
   }
+
+  private getUserRole(): Observable<'manager' | 'member' | 'manager&member' | 'pendingMember' | 'outsider'> {
+    return combineLatest([this.isManager$, this.isMember$, this.isPendingMember$]).pipe(
+      map(([isManager, isMember, isPendingMember]) => {
+        switch (true) {
+          case isManager:
+            return 'manager';
+          case isMember:
+            return 'member';
+          case isManager && isMember:
+            return 'manager&member';
+          case isPendingMember:
+            return 'pendingMember';
+          default:
+            return 'outsider';
+        }
+      })
+    );
+  }
+
   public goToCreateProject() {
     this.router.navigate(['projects', 'new'], { relativeTo: this.route });
   }
