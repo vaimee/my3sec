f0413df54884eb733579ead339e0ecf423ef21dc
ivan <idimitryz@gmail.com>
2023-07-07 12:28:13 +0200

    fix(frontend): get all skills (not only the last)

--- frontend/src/app/shared/services/skill.service.ts
+++ frontend/src/app/shared/services/skill.service.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
-import { Observable, concatMap, forkJoin, map, mergeMap, switchMap, toArray } from 'rxjs';
+import { Observable, catchError, concatMap, forkJoin, map, mergeMap, switchMap, toArray } from 'rxjs';
 
 import { Injectable } from '@angular/core';
 
@@ -45,19 +45,21 @@ export class SkillService {
         return forkJoin(requests);
       }),
       concatMap(data => data),
-      switchMap(([skill, progress]) =>
-        this.getSkill(skill.toNumber()).pipe(map(skillData => ({ ...skillData, progress: progress.toNumber() })))
-      ),
+      mergeMap(([skillId, progress]) => {
+        return this.getSkill(skillId.toNumber()).pipe(
+          map(skillData => ({ ...skillData, progress: progress.toNumber() }))
+        );
+      }),
       toArray()
     );
   }
 
   public getSkill(id: number): Observable<Skill> {
-    console.log('getSkill', id);
     return this.skillRegistry.getSkill(id).pipe(
-      switchMap(({ id, metadataURI }) => {
-        console.log('here');
-        return this.ipfs.retrieveSkill(id.toNumber(), metadataURI.replace('ipfs://', ''));
+      switchMap(({ id, metadataURI }) => this.ipfs.retrieveSkill(id.toNumber(), metadataURI.replace('ipfs://', ''))),
+      catchError(err => {
+        console.log(err);
+        throw Error('error getting skill');
       })
     );
   }
