7fa272084c69468bf6415fae0ea89ea210e67901
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2023-07-17 17:13:59 +0200

    style(protocol/events): cleanup comments

--- protocol/contracts/common/libraries/Events.sol
+++ protocol/contracts/common/libraries/Events.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.9;
 
 library Events {
     event ProfileCreated(uint256 indexed profileId, address indexed owner);
-    event EnergyGiven(uint256 indexed fromProfileId, uint256 indexed toProfileId, uint256 amount); /** x */
-    event EnergyRemoved(uint256 indexed fromProfileId, uint256 indexed toProfileId, uint256 amount); /** x */
+    event EnergyGiven(uint256 indexed fromProfileId, uint256 indexed toProfileId, uint256 amount);
+    event EnergyRemoved(uint256 indexed fromProfileId, uint256 indexed toProfileId, uint256 amount);
     event OrganizationRegistered(address indexed organization);
-    event OrganizationJoined(address indexed organization, uint256 indexed profileId); /** x */
-    event OrganizationLeft(address indexed organization, uint256 indexed profileId); /** x */
+    event OrganizationJoined(address indexed organization, uint256 indexed profileId);
+    event OrganizationLeft(address indexed organization, uint256 indexed profileId);
     event PendingMemberApproved(address indexed organization, uint256 indexed profileId);
     event PendingMemberRejected(address indexed organization, uint256 indexed profileId);
     event ProjectCreated(address indexed organization, uint256 indexed projectId);
@@ -19,7 +19,7 @@ library Events {
     event TaskMemberAdded(address indexed organization, uint256 indexed taskId, uint256 indexed profileId);
     event TaskMemberRemoved(address indexed organization, uint256 indexed taskId, uint256 indexed profileId);
     event TimeLogged(uint256 indexed profileId, uint256 time);
-    event ExperienceWithdrawn(address indexed organization, uint256 indexed taskId, uint256 indexed profileId); /** x */
+    event ExperienceWithdrawn(address indexed organization, uint256 indexed taskId, uint256 indexed profileId);
     event SkillCreated(uint256 indexed skillId);
     event CertificateIssued(address from, uint256 indexed profileId, uint256 indexed certificateId);
 }
